49999ec84377c64762683581e5466d537fda7b6d
Adam Stevens <ajstevens@ajstevens-mbp.local>
2017-05-11 15:23:50 +0100

    Updating

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Welcome to iciandme!');
+  res.send('Hello World!');
 });
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on', http.address().port);
