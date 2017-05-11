82c723221b43e8affac88b5f17af961929fd85a3
Adam Stevens <ajstevens@ajstevens-mbp.local>
2017-05-11 15:52:41 +0100

    Test

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
 app.get('/webhook', function (req, res) {
   if (req.query['hub.verify_token'] === 'ici_and_me_verification') {
     res.send(req.query['hub.challenge']);
