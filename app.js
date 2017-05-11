80eb0abfee6e8cd75febf335af57d83cd5b5bac1
Adam Stevens <ajstevens@ajstevens-mbp.local>
2017-05-11 15:55:05 +0100

    Test

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
-app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === 'ici_and_me_verification') {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Error, wrong validation token');
-  }
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
+// app.get('/webhook', function (req, res) {
+//   if (req.query['hub.verify_token'] === 'ici_and_me_verification') {
+//     res.send(req.query['hub.challenge']);
+//   } else {
+//     res.send('Error, wrong validation token');
+//   }
+// });
