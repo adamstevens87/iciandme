bfd9ce0fd1d9f089ec51a3f77764d6914bd16f0c
Adam Stevens <ajstevens@ajstevens-mbp.local>
2017-05-11 14:19:24 +0100

    Updating build

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-var express = require('express')
-var app = express()
-
+var express = require('express');
+var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
+  res.send('Hello World!');
+});
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
+  console.log('Example app listening on port 3000!');
+});
 app.get('/webhook', function (req, res) {
   if (req.query['hub.verify_token'] === 'ici_and_me_verification') {
     res.send(req.query['hub.challenge']);
