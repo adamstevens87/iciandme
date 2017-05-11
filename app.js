6bb73dbd5291ef772ab529b92076970e29e715ae
Adam Stevens <ajstevens@ajstevens-mbp.local>
2017-05-11 16:19:43 +0100

    Test

--- app.js
+++ app.js
@@ -7,70 +7,16 @@ app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-
-// App Secret can be retrieved from the App Dashboard
-const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
-  process.env.MESSENGER_APP_SECRET :
-  config.get('appSecret');
-
 // Arbitrary value used to validate a webhook
 const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
   (process.env.MESSENGER_VALIDATION_TOKEN) :
   config.get('validationToken');
 
-// Generate a page access token for your page from the App Dashboard
-const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-  config.get('pageAccessToken');
 
-// URL where the app is running (include protocol). Used to point to scripts and
-// assets located at this address.
-const SERVER_URL = (process.env.SERVER_URL) ?
-  (process.env.SERVER_URL) :
-  config.get('serverURL');
-
-if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
-  console.error("Missing config values");
-  process.exit(1);
-}
-
-/*
- * Use your own validation token. Check that the token used in the Webhook
- * setup is the same token used here.
- *
- */
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    res.send(req.query['hub.challenge']);
   } else {
-    console.error("Failed validation. Make sure the validation tokens match." + VALIDATION_TOKEN + ", " + req.query['hub.verify_token']);
-    res.sendStatus(403);
+    res.send('Error, wrong validation token');
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-// // Arbitrary value used to validate a webhook
-// const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-//   (process.env.MESSENGER_VALIDATION_TOKEN) :
-//   config.get('validationToken');
-//
-//
-// app.get('/webhook', function (req, res) {
-//   if (req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-//     res.send(req.query['hub.challenge']);
-//   } else {
-//     res.send('Error, wrong validation token');
-//   }
-// });
