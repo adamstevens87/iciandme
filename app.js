579281d73c9dd238db931dc17f58ac04c2cc98b9
Adam Stevens <ajstevens@ajstevens-mbp.local>
2017-05-11 15:37:52 +0100

    Refresh

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
 });
-// app.get('/webhook', function (req, res) {
-//   if (req.query['hub.verify_token'] === 'ici_and_me_verification') {
-//     res.send(req.query['hub.challenge']);
-//   } else {
-//     res.send('Error, wrong validation token');
-//   }
-// });
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === 'ici_and_me_verification') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');
+  }
+});
